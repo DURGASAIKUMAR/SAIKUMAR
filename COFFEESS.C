f2112465b29c59d4b8f4007e1b020088f987cacf
DURGASAIKUMAR <53969807+DURGASAIKUMAR@users.noreply.github.com>
2019-08-10 16:58:41 +0530

    Update COFFEESS.C

--- COFFEESS.C
+++ COFFEESS.C
@@ -4,13 +4,21 @@ void main()
 int x,y,p,rupees=0;
 printf("enter number of rupees:");
 scanf("%d",&x);
+  if(x>=0 && x<=10000)
+  {
 printf("enter the percentage of discount:");
 scanf("%d",&p);
+  if(p>=1 && p<=100)
+  {
 while(x>1)
 {
 y=((x/100.0)*p);
 rupees+=x;
 x=x-y;
 }
-printf("%d",rupees+1);
+    printf("%d",rupees+1);
+  }
+  }
+else
+  printf("offer is invalid");
 }
